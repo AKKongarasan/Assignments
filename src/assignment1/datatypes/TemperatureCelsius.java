be8a9a317e7e69861c4287cc692feba9b6b1ceab
AKKongarasan <asokkongu@gmail.com>
2025-04-09 12:12:51 +0530

    Data type Assignment

---
+++ src/assignment1/datatypes/TemperatureCelsius.java
@@ -0,0 +1,21 @@
+package assignment1.datatypes;
+
+public class TemperatureCelsius {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Temperature of a city in degree celsius : 25.5
+		//Float data type used for Decimal value 
+		
+		float celsius = 25.5f;
+		System.out.println("Temperature of a chennai city in degree Celsius:" + celsius);
+		
+		//Customer placed an order 
+		
+		boolean order = true;
+		System.out.println("Customer has placed order:" + order);;
+
+	}
+
+}
