881cdb343ed85a5cd17bb60c79e38e0f21d7a9af
AKKongarasan <asokkongu@gmail.com>
2025-04-09 22:57:55 +0530

    data types

---
+++ src/arrays/Array.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] empnames = new String[3];
+		
+		empnames[1] = "AK";
+		empnames[1] = "sekar";
+		empnames[2] = "LK";
+		
+		System.out.println(empnames.length);
+		System.out.println(empnames[1]);
+		System.out.println(empnames[1]);
+		System.out.println(empnames[2]);
+		
+		String [][] projects = new String[2][3];
+		projects[0][2] = "AK";
+		projects[1][1]= "sekar";
+		
+		
+		String [][][] companies = new String[3][2][4];
+		companies[0][1][2]= "Ak";
+		
+
+	}
+
+}
--- src/assignment1/datatypes/TemperatureCelsius.java
+++ src/assignment1/datatypes/TemperatureCelsius.java
@@ -12,9 +12,67 @@ public class TemperatureCelsius {
 		System.out.println("Temperature of a chennai city in degree Celsius:" + celsius);
 		
 		//Customer placed an order 
+		//boolean is data type used for boolean values
 		
 		boolean order = true;
-		System.out.println("Customer has placed order:" + order);;
+		System.out.println("Customer has placed order:" + order);
+		
+		
+		//Person's Phone number
+		//String data type used for characters
+		
+		String phone = "975-1177-337";
+		System.out.println("My Phone Number" + phone );
+		
+		
+		//Amount of money in ban k account 
+		//Float data type used for Decimal value 
+		
+		
+		float account = 1000.50f;
+		System.out.println( "Amount of money in bank account:" + account);
+		
+		//Person's email address
+		//String data type used for characters
+		
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address:" + email );
+		
+		//Martial status
+		//boolean is data type used for boolean values
+		
+		boolean martialstatus = true;
+		System.out.println("Preson's martial status:" + martialstatus);
+		
+		//Person's occupation
+		//String data type used for characters
+		
+		String occupation = "softwar engineer";
+		System.out.println("Person' occupation:" + occupation );
+	
+		String color = "blue";
+		System.out.println("Person' occupation:" + color );
+		
+		int year = 2023;
+		System.out.println("current year :"+ year );
+		
+		
+		//Number of followers
+		
+		String followers = "1,000,000";
+		System.out.println("Number of follower on a social media:"+ followers );
+		
+		
+		// Rating of a movie
+		
+		float movie = 7.5f;
+		System.out.println( "rating of a movie:" + movie);
+		
+		
+		char bloodtype = 'A';
+		System.out.println("Person's blood type:" + bloodtype );
+		
+		
 
 	}
 
